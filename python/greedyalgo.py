d5ef739399cfd7e186c9c0fc85614b65b5cb224e
Kyle Baumgarten <BaumDev@proton.me>
2023-08-03 01:09:06 -0400

    selecting stations

--- python/greedyalgo.py
+++ python/greedyalgo.py
@@ -1,5 +1,9 @@
 states_needed = set(["mt", "wa", "or", "id", "nv", "ut", "ca", "az"])
 
+#How you would actually do this:
+state_need_ls=["mt", "wa", "or", "id", "nv", "ut", "ca", "az"]
+state_need_set=set(state_need_ls)
+
 stations = {}
 stations["kone"] = set(["id", "nv", "ut"])
 stations["ktwo"] = set(["wa", "id", "mt"])
@@ -9,10 +13,27 @@ stations["kfive"] = set(["ca", "az"])
 
 final_stations = set()
 
-best_station = None
-states_covered = set()
-for station, states in stations.items():
-    covered = states_needed & states
-    if len(covered) > len(states_covered):
-        best_station = station
-        states_covered = covered
+#best_station = None
+solution_found=False
+selected_stations=[]
+while solution_found==False:
+
+    best_station=None
+    states_covered = set()
+    for station, states in stations.items():
+        covered = states_needed & states
+        if len(covered) > len(states_covered):
+            best_station = station
+            states_covered = covered
+    stations.pop(best_station)
+    selected_stations.append(best_station)
+    states_needed=states_needed-set(states_covered)
+    if states_needed==set([]):
+        solution_found=True
+        break
+
+    print("Stations: "+str(stations))
+    print("Needed Still: "+str(states_needed))
+print("Final stations: "+str(selected_stations))
+
+

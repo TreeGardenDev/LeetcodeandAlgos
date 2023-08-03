b3938a8323ae959debaec4ba68ec01085810cd63
Kyle Baumgarten <BaumDev@proton.me>
2023-08-03 01:17:20 -0400

    function and protect

--- python/greedyalgo.py
+++ python/greedyalgo.py
@@ -1,3 +1,30 @@
+def greedy_select(states_needed, stations):
+    
+    solution_found=False
+    selected_stations=[]
+    while solution_found==False:
+    
+        best_station=None
+        states_covered = set()
+        for station, states in stations.items():
+            covered = states_needed & states
+            if len(covered) > len(states_covered):
+                best_station = station
+                states_covered = covered
+        if best_station==None:
+            solution_found=True
+            return "Selected Stations: "+str(selected_stations)+" Remaining States Requiring Station: "+str(states_needed)
+        stations.pop(best_station)
+        selected_stations.append(best_station)
+        states_needed=states_needed-set(states_covered)
+        if states_needed==set([]):
+            solution_found=True
+            break
+    
+    return selected_stations
+
+    
+    
 states_needed = set(["mt", "wa", "or", "id", "nv", "ut", "ca", "az"])
 
 #How you would actually do this:
@@ -13,27 +40,9 @@ stations["kfive"] = set(["ca", "az"])
 
 final_stations = set()
 
+selected_stations=greedy_select(states_needed, stations)
+print(str(selected_stations))
+
 #best_station = None
-solution_found=False
-selected_stations=[]
-while solution_found==False:
-
-    best_station=None
-    states_covered = set()
-    for station, states in stations.items():
-        covered = states_needed & states
-        if len(covered) > len(states_covered):
-            best_station = station
-            states_covered = covered
-    stations.pop(best_station)
-    selected_stations.append(best_station)
-    states_needed=states_needed-set(states_covered)
-    if states_needed==set([]):
-        solution_found=True
-        break
-
-    print("Stations: "+str(stations))
-    print("Needed Still: "+str(states_needed))
-print("Final stations: "+str(selected_stations))
 
 
